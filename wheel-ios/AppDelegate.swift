52fe22dd6a1211fb896e2246b919be618107dfff
CathyWrHua <cathy.hua@gmail.com>
2018-02-03 23:19:36 -0800

    convert to swift

---
+++ wheel-ios/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  wheel-ios
+//
+//  Created by Cathy Hua on 2018-02-03.
+//  Copyright © 2018 Cathy Hua. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_application:UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
